f0cfcfa20e30a46992f0ce68cabfe0f380cd5a01
4NDXIN <101297390+andxinn@users.noreply.github.com>
2022-12-05 03:02:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6282287219167']
-global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
+global.owner = ['6287710240822']
+global.ownernomer = "6287710240822"
+global.premium = ['6287710240822']
 global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.author = 'Xinn Bot'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
